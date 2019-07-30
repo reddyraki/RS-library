032f19669e51e47ab5f9ed87798a16ca9bb3d8a2
Rakesh <Rakesh@DESKTOP-EO5UIOV>
2019-07-30 21:31:57 +0530

    mater commit

--- src/main/java/com/controllers/ControllersOfLibrary.java
+++ src/main/java/com/controllers/ControllersOfLibrary.java
@@ -36,37 +36,39 @@ public class ControllersOfLibrary {
 	}
 
 	@RequestMapping(value = "/LoginUser")
-	public String loginUser(@RequestParam("UserName") String UserName,@RequestParam ("Password") String Password,  Model model)  {
-		
-     System.out.println("in loginUserMethod");
+	public String loginUser(@RequestParam("UserName") String UserName, @RequestParam("Password") String Password,
+			Model model) {
+
+		System.out.println("in loginUserMethod");
 		// hibernate query for to check login details
 
 		Configuration configure = new Configuration().configure();
 		SessionFactory sf = configure.buildSessionFactory();
 		Session sess = sf.openSession();
 		Query query = sess.createQuery("from RegistrationPojo  where email = :email and password=:password");
-		
+
 		System.out.println("it was hitting database");
-		
-		 query.setParameter("email", UserName);
-		 query.setParameter("password", Password);
-		 
+
+		query.setParameter("email", UserName);
+		query.setParameter("password", Password);
+
 		List list2 = query.list();
-		
+
 		sess.beginTransaction().commit();
 		sess.close();
-		if(list2.isEmpty()){
-		System.out.println("the entered values are in correct");
-		return "login";
-		}else
+		if (list2.isEmpty()) {
+			System.out.println("the entered values are in correct");
+			return "login";
+		} else
 			System.out.println("the entered values are correct");
 		return "forward";
-		
+
 	}
+
 	@RequestMapping(value = "/ReadingBookDetails")
-	public String readingBook(  Model model) {
+	public String readingBook(Model model) {
 		System.out.println("this is in reading bok method ");
- 
+
 		// need to hibernate logic to get the book total details from the
 		// database
 		Configuration configure = new Configuration().configure();
@@ -79,18 +81,19 @@ public class ControllersOfLibrary {
 		List list1 = query.list();
 		sess.close();
 		System.out.println("the database hitted got the data from the database");
-	System.out.println("the book name is "+list1.get(0));
-		
+		System.out.println("the book name is " + list1.get(0));
+
 		model.addAttribute("list", list1);
-		
+
 		return "profile";
 	}
-@RequestMapping(value="/RigesterNewUser")
-	public String register(){
-	
-	System.out.println("on the way new registration jsp page");
+
+	@RequestMapping(value = "/RigesterNewUser")
+	public String register() {
+
+		System.out.println("on the way new registration jsp page");
 		return "Registration";
-		
-		
+
 	}
+	public
 }
